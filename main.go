4bbcee8210bf08c87887a3a2d282d559dec73594
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2017-05-21 18:03:37 +0200

    commands map don't match with inputs from raw slack text

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"io/ioutil"
 	"strings"
 	//"bytes"
+	//"strconv"
 )
 
 var api = slack.New("xoxb-184523172850-7nbbBBEc0gvjSXaBFVq7Fqtk")
 
 var gifs = make(map[string]string)
+var sanity = make(map[string]int)
 
 func panicCheck(e error) {
 	if e != nil {
@@ -18,11 +20,11 @@ func panicCheck(e error) {
 	}
 }
 
-/* func warningCheck(e error) {
+func warningCheck(e error) {
 	if e != nil {
 		fmt.Printf("Warning : %s\n", e)
 	}
-} */
+}
 
 func splitGifLine(line string, gifTuple chan []string) {
 	gifTuple <- strings.Split(line, "=")
@@ -41,6 +43,31 @@ func initGifs() {
 		tuple := <- gifsTuples
 		if len(tuple) > 1 {
 			gifs[tuple[0]] = tuple[1]
+		}
+	}
+	fmt.Printf("gifs :\n%s\n", gifs)
+}
+
+func initSanity() {
+	/* data, e := ioutil.ReadFile("sanity.txt")
+	panicCheck(e)
+	lines := strings.Split(string(data), "\n")
+	for i := 0; i < len(lines); i++ {
+		tuple := strings.Split(lines[i], "=")
+		if len(tuple) > 1 {
+			points, _ := strconv.Atoi(tuple[1])
+			sanity[tuple[0]] = points
+		}
+	}
+	fmt.Printf("sanity :\n%s\n", sanity) */
+	users, e := api.GetUsers()
+	if e != nil {
+		warningCheck(e)
+		return
+	}
+	for i := 0; i < len(users); i++ {
+		if users[i].IsBot == false && users[i].Deleted == false {
+			sanity[users[i].Name] = 10
 		}
 	}
 }
@@ -66,17 +93,55 @@ func messageEventGifPoster(message string, meme string, channel string) {
 }
 
 func messageEvent(message *slack.MessageEvent) {
-	for meme := range gifs {
-		go messageEventGifPoster(message.Text, meme, message.Channel)
+	text := message.Text
+	if strings.Contains(message.Text, "<@U5EFD52R0>") == true || message.Channel[0] == 'D' {
+		text = strings.Replace(text, "<@U5EFD52R0>", "", 1)
+		text = strings.TrimSpace(text)
+		words := strings.Split(text, "=")
+		if len(words) > 0 {
+			if val, ok := commands[words[0]]; ok {
+				go val.(func(*slack.MessageEvent))(message)
+			}
+		}
 	}
+	for meme := range gifs {
+		go messageEventGifPoster(text, meme, message.Channel)
+	}
+}
+
+func helpCommand(message *slack.MessageEvent) {
+	params := slack.PostMessageParameters{}
+	params.AsUser = true
+	params.UnfurlLinks = true
+	params.UnfurlMedia = true
+	api.PostMessage(message.Channel, "https://media.giphy.com/media/WfE3yNXrkMAAo/giphy.gif?response_id=5921b583a79f5c11408fde74", params)
+	api.PostMessage(message.Channel, "-1 en sanité pour " + message.Username, params)
+	if user, ok := sanity[message.Username]; ok {
+		sanity[message.Username] = user - 1
+	} else {
+		sanity[message.Username] = 9
+	}
+}
+
+func sanityCommand(message *slack.MessageEvent) {
+	text := "Sanity board :\n"
+	for user, points := range(sanity) {
+		text = text + user + " : " + string(points) + "\n"
+	}
+	simpleMessagePoster(message.Channel, text)
+}
+
+var commands = map[string]interface{}{
+	"help": helpCommand,
+	"sanity": sanityCommand,
 }
 
 func main() {
 	api.SetDebug(true)
 	_, err := api.AuthTest()
 	panicCheck(err)
-	initGifs()
-	fmt.Printf("gifs :\n%s\n", gifs)
+	go initGifs()
+	go initSanity()
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 	for msg := range rtm.IncomingEvents {
